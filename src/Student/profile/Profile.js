377d106191fd3d3ceb1d1f6aa35e0b2f484a168c
Mayur Dayal <mayur.dayal97@gmail.com>
2021-10-05 15:15:46 +0530

    updated the ui of the profile and change password page

--- src/Student/profile/Profile.js
+++ src/Student/profile/Profile.js
@@ -1,13 +1,11 @@
-import { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Studentnav from '../studentnav';
-import axios from 'axios';
-import Config from '../../config.json';
-import Styles from '../studentdashboard.module.css';
-import dateFormat from 'dateformat';
-import image from './user 1.png'
-import './profile.css'
-class Profile extends Component {
+import React from "react";
+import axios from "axios";
+import Config from "../../config.json";
+import Studentnav from "../studentnav";
+import Footer from "../Footer";
+import "./profile.css";
+
+class Profile extends React.Component {
   constructor() {
     super();
     let data = localStorage.getItem("userdetail");
@@ -15,45 +13,50 @@ class Profile extends Component {
     this.state = {
       token: data.token,
       student: data.username,
-      student_details: '',
-      school_details: '',
-      section_details: '',
-      classname: '',
-      email: '',
-      subjects: []
-    }
-
+      student_details: "",
+      school_details: "",
+      section_details: "",
+      classname: "",
+      email: "",
+      subjects: [],
+    };
   }
   componentDidMount = () => {
     var formdata = new FormData();
     this.setState({ isloading: true });
-    formdata.append('page_name', 'Student-Profile');
+    formdata.append("page_name", "Student-Profile");
     var requestOptions = {
-      method: 'POST',
+      method: "POST",
       body: formdata,
-      redirect: 'follow',
+      redirect: "follow",
       headers: {
-        'Authorization': 'Token ' + this.state.token
+        Authorization: "Token " + this.state.token,
       },
     };
     fetch(Config.SERVER_URL + "users/post-analytics/", requestOptions)
-      .then(response => response.json())
-      .then(json => {
+      .then((response) => response.json())
+      .then((json) => {
         console.log(json);
       })
-      .catch(error => {
-        this.props.history.push('/error');
+      .catch((error) => {
+        this.props.history.push("/error");
       });
     var requestOptions1 = {
-      redirect: 'follow',
+      redirect: "follow",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Token ' + this.state.token
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Token " + this.state.token,
       },
     };
-    axios.get(Config.SERVER_URL + 'users/get-student-details/?username=' + this.state.student, requestOptions1)
-      .then(response => {
+    axios
+      .get(
+        Config.SERVER_URL +
+        "users/get-student-details/?username=" +
+        this.state.student,
+        requestOptions1
+      )
+      .then((response) => {
         console.log(response.data);
         var ch = response.data;
         this.setState({
@@ -65,79 +68,107 @@ class Profile extends Component {
           section_details: ch.student_data.section_assoc,
         });
       })
-      .catch(error => {
-        this.props.history.push('/error');
+      .catch((error) => {
+        this.props.history.push("/error");
       });
-  }
+  };
   emailchange = (event) => {
     var variable = event.target.value;
     console.log(variable);
     this.setState({
       email: variable,
-    })
-  }
+    });
+  };
   submit_email = (e) => {
     e.preventDefault();
     console.log(this.state);
     var formdata = new FormData();
     formdata.append("email", this.state.email);
     var requestOptions = {
-      method: 'PUT',
-      redirect: 'follow',
+      method: "PUT",
+      redirect: "follow",
       body: formdata,
       headers: {
-        'Authorization': 'Token ' + this.state.token
+        Authorization: "Token " + this.state.token,
       },
     };
     fetch(Config.SERVER_URL + "users/update-student-email/", requestOptions)
-      .then(response => response.json())
-      .then(json => {
+      .then((response) => response.json())
+      .then((json) => {
         console.log(json);
         alert(json.Success);
         window.location.reload();
       })
-      .catch(error => {
-        this.props.history.push('/error');
+      .catch((error) => {
+        this.props.history.push("/error");
       });
-  }
+  };
   render() {
-    var dateFormat = require("dateformat");
     return (
-      <div>
+      <>
         <Studentnav />
-        <div className="container">
-          <div className="row" style={{ marginTop: '50px' }}>
-            <div className="col-md-12">
-              <div class="title">
-                <p style={{ fontFamily: 'Montserrat', fontWeight: 'bold', fontSize: '20px', display: 'inline-block' }}>Student Profile</p>
-              </div>
-              <center>
-                <div>
-                  <div class="profile_box">
-                    <div class="profile_inner_box">
-                      <img src={image} style={{ width: '70px', height: '70px' }} />
-                    </div>
-                    <div style={{ marginTop: '70px', }}>
-                      <div className='profile_data'>Student Name : {this.state.student_details.name}</div>
-                      <div className='profile_data'>Student Email : {this.state.student_details.email}</div>
-                      <div className='profile_data'>Mobile Number : {this.state.student_details.mobile_number}</div>
-                      <div className='profile_data'>Class Name : {this.state.classname}</div>
-                      {this.state.subjects.map((item, index) => (
-                        <div className='profile_data'>Subject {index + 1}: {item.subject_assoc.subject_name}</div>
-                      ))}
-                      <div className='profile_data'>Section Name : {this.state.section_details.section_name}</div>
-                      <div className='profile_data'>School Name : {this.state.school_details.name}</div>
-                      <div className='profile_data'>Student Validity : ---------</div>
-                    </div>
-                  </div>
-                </div>
-              </center>
-            </div>
-          </div>
-        </div>
-        <br />
-      </div>
 
+        <div className="profile-container">
+          <h2>Student Profile</h2>
+
+          <fieldset className="profile-box-contain">
+            <legend>
+              <div className="profile-icon">
+                <i className="fas fa-user"></i>
+              </div>
+            </legend>
+            <div className="profile-box-content">
+              <div>
+                <p className="prof-lt">Student Name:</p>
+                <p className="prof-rt">{this.state.student_details.name}</p>
+              </div>
+
+              <div>
+                <p className="prof-lt">Email Id:</p>
+                <p className="prof-rt">{this.state.student_details.email}</p>
+              </div>
+
+              <div>
+                <p className="prof-lt">Mobile Number:</p>
+                <p className="prof-rt">
+                  {this.state.student_details.mobile_number}
+                </p>
+              </div>
+
+              <div>
+                <p className="prof-lt">Class Name:</p>
+                <p className="prof-rt">{this.state.classname}</p>
+              </div>
+
+              {this.state.subjects.map((item, index) => (
+                <div>
+                  <p className="prof-lt">Subject {index + 1}:</p>
+                  <p className="prof-rt">{item.subject_assoc.subject_name}</p>
+                </div>
+              ))}
+
+              <div>
+                <p className="prof-lt">Section Name:</p>
+                <p className="prof-rt">
+                  {this.state.section_details.section_name}
+                </p>
+              </div>
+
+              <div>
+                <p className="prof-lt">School Name:</p>
+                <p className="prof-rt">{this.state.school_details.name}</p>
+              </div>
+
+              <div>
+                <p className="prof-lt">Student Validity:</p>
+                <p className="prof-rt">---------</p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+
+        <Footer />
+      </>
     );
   }
 }
